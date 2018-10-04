b203d0bf779247640e22518b5dbf30f219487add
Shepherd, Marc M <marc-shepherd@uiowa.edu>
2018-10-03 19:16:00 -0500

    Upload New File

---
+++ oral_exam1/S10_ArabicToRomanGUI/ArabicAndRomanConversion.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+import javax.swing.*;
+
+/**
+ * This assignment is part of the homework grid listed in Introduction to Software Design, taught by Professor Casavant.
+ * This program converts arabic numbers to equivalent roman numerals, and vice versa, all on
+ * a smooth graphical user interface, allowing you to first convert as much arabic to roman as you would like,
+ * followed by as much roman to arabic as you would like.
+ * @author Marc Shepherd
+ * @version 1.0, 9/30/2018
+ * @since SDK 1.8
+ */
+public class ArabicAndRomanConversion {
+
+    public static void main(String[] args) {
+        Swing arabicRomanGUI = new Swing(); //New Swing class to facilitate conversion
+        arabicRomanGUI.display(); //GUI to display conversions
+
+    }
+}
